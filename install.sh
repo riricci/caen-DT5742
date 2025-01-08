84239c30b55a9f91427d41604478419481f6cfb2
eic <eic@epiclab03.bo.infn.it>
2025-01-08 17:55:15 +0100

    added install.sh script for all the repository

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+echo "installing CAENUSBdrvB. Progressing..."
+cd dependencies/CAENUSBdrvB-v1.6.0/
+./install.sh
+echo "  CAENUSBdrvB installed successfully!"
+
+echo "installing CAENVMELib. Progressing..."
+cd ../../dependencies/CAENVMELib-v4.0.2/lib/
+sh install_x64
+echo "  CAENVMELib installed successfully!"
+
+echo "installing CAENComm. Progressing..."
+cd ../../../dependencies/CAENComm-v1.7.0/lib/
+sh install_x64
+echo "  CAENComm installed successfully!"
