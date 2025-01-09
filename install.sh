3d2824c26b7991de248b0514923bd429f6aa5e26
eic <eic@epiclab03.bo.infn.it>
2025-01-09 18:07:31 +0100

    added options for pyenv

--- install.sh
+++ install.sh
@@ -1,36 +1,39 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# activating environment for Python 
-echo "Creating a Python virtual environment..."
-python3 -m venv myenv
+# activating environment for Python --> uncomment first two lines if you do not have any environment
+# echo "Creating a Python virtual environment..."
+# python3 -m venv ../myenv
 echo "Activating the virtual environment..."
-source myenv/bin/activate
+source ../myenv/bin/activate
 
 # installing dependencies
 echo -e "\033[34minstalling CAENUSBdrvB. Progressing...\033[0m"
 cd dependencies/CAENUSBdrvB-v1.6.0/
-./install.sh
+sudo ./install.sh
 echo -e "\033[32m  CAENUSBdrvB installed successfully!\033[0m"
 
 echo -e "\033[34minstalling CAENVMELib. Progressing...\033[0m"
 cd ../../dependencies/CAENVMELib-v4.0.2/lib/
-sh install_x64
+sudo sh install_x64
 echo -e "\033[32m  CAENVMELib installed successfully!\033[0m"
 
 echo -e "\033[34minstalling CAENComm. Progressing...\033[0m"
 cd ../../../dependencies/CAENComm-v1.7.0/lib/
-sh install_x64
+sudo sh install_x64
 echo -e "\033[32m  CAENComm installed successfully!\033[0m"
+
+pip install plotly flask pandas 
+
 
 # installing CAEN digitizer libraries
 echo -e "\033[34minstalling CAENComm. Progressing...\033[0m"
 cd ../../../dependencies/CAENComm-v1.7.0/lib/
-sh install_x64
+sudo sh install_x64
 echo -e "\033[32m  CAENComm installed successfully!\033[0m"
 
 echo -e "\033[34minstalling CAENDigitizer library. Progressing...\033[0m"
 cd ../../../digitizerDT5742/CAENDigitizer-v2.17.3/lib/
-sh install_x64
+sudo sh install_x64
 echo -e "\033[32m  CAENDigitizer library installed successfully!\033[0m"
 
 echo -e "\033[34minstalling CAENDigitizer python library. Progressing...\033[0m"
