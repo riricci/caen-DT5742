dbf6880a4b0e847f0f4f9302ae7593800e5324f9
eic <eic@epiclab03.bo.infn.it>
2025-02-06 15:09:08 +0100

    updated with some buttons for sampling frequency

--- scripts_py/rwaveclient_example.py
+++ scripts_py/rwaveclient_example.py
@@ -1,8 +1,9 @@
 import sys
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QWidget, QVBoxLayout, QPushButton
 import pyqtgraph as pg
 from rwave import rwaveclient
+import threading
 
 host = 'localhost'
 port = 30001
@@ -10,10 +11,28 @@ port = 30001
 class OscilloscopeApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.host = 'localhost'  # Aggiungi questa riga
+        self.port = 30001  # Aggiungi questa riga
+        self.current_frequency = 750
         self.initUI()
+        
+    def set_frequency(self, freq):
+        self.current_frequency = freq
+        threading.Thread(target=self.configure_digitizer, daemon=True).start()
+
+    def configure_digitizer(self):
+        with rwaveclient(self.host, self.port, verbose=True) as rwc:
+            if rwc is not None:
+                rwc.send_cmd(f'sampling {self.current_frequency}')
+                print(f"Configured digitizer to {self.current_frequency} MHz")    
+                rwc.send_cmd('grmask 0x1')
+                rwc.send_cmd('chmask 0x0003')
+
     
     def initUI(self):
         layout = QVBoxLayout()
+        self.setWindowTitle("Digitizer Control")
+        self.setGeometry(100, 100, 800, 600)
         
         # Graph widget
         self.plotWidget = pg.PlotWidget()
@@ -32,6 +51,17 @@ class OscilloscopeApp(QWidget):
         self.button.clicked.connect(self.startAcquisition)
         layout.addWidget(self.button)
         
+        self.freq_buttons = []
+        for freq in [750, 1000, 2500, 5000]:  # Frequencies in MHz
+            btn = QPushButton(f"{freq} MHz", self)
+            btn.clicked.connect(lambda _, f=freq: self.set_frequency(f))
+            self.freq_buttons.append(btn)
+            
+        freq_layout = QHBoxLayout()
+        for btn in self.freq_buttons:
+            freq_layout.addWidget(btn)
+        layout.addLayout(freq_layout)
+        
         self.setLayout(layout)
         self.setWindowTitle("Oscilloscope Viewer")
     
@@ -42,14 +72,12 @@ class OscilloscopeApp(QWidget):
             self.plotData(data)
         self.button.setEnabled(True)
     
+    
     def acquireData(self):
         with rwaveclient(host, port, verbose=True) as rwc:
             if rwc is None:
                 return None
             # Configure acquisition
-            rwc.send_cmd('sampling 750')
-            rwc.send_cmd('grmask 0x1')
-            rwc.send_cmd('chmask 0x0003')
             rwc.send_cmd("start")
             rwc.send_cmd('swtrg 1024')
             rwc.send_cmd('readout')
