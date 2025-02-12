d7344f77f10d640b0508ab8c42341ac32f7cd1c0
eic <eic@epiclab03.bo.infn.it>
2025-02-12 11:33:43 +0100

    added if block for server activation

--- configure.sh
+++ configure.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# activating environment for Python
 
+# Activating environment for Python
 echo "Activating the virtual environment..."
 source ../myenv/bin/activate
+
+# Check for the flag --start-servers
+if [[ "$1" == "--servers" ]]; then
+    echo "Starting servers..."
+    nohup /eu/aimtti/aimtti-server.py --address aimtti-tgp3152-00 &
+    nohup /eu/caen-dt5742b/soft/bin/rwaveserver &
+else
+    echo "Skipping server startup."
+fi
