1d1f19afeeb3930365e598916929d714e78ac413
eic <eic@epiclab03.bo.infn.it>
2025-02-14 08:49:16 +0100

    bugfix

--- configure.sh
+++ configure.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-# 2025-02-12, Riccardo Ricci
-# usage:  
-#   source configure.sh # to activate the virtual environment
-#   source configure.sh --servers # to activate the virtual environment and start the servers
 
-
-# Activating environment for Python
+# activating environment for Python
 echo "Activating the virtual environment..."
 source ../myenv/bin/activate
 
-# Check for the flag --start-servers
-if [[ "$1" == "--servers" ]]; then
-    echo "Starting servers..."
-    nohup /eu/aimtti/aimtti-server.py --address aimtti-tgp3152-00 &
-    nohup /eu/caen-dt5742b/soft/bin/rwaveserver &
-else
-    echo "Skipping server startup."
-fi
+# running the server for the pulser and the digitizer
+rnohup "/eu/aimtti/aimtti-server.py --address aimtti-tgp3152-00"
+rnohup /eu/caen-dt5742b/soft/bin/rwaveserver
